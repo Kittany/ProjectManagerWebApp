649fb5a85bec62579d689dfc4b31f0556927469f
Hakam Mssarwe <HakamStudent@gmail.com>
2021-02-06 19:41:14 +0200

    Removed database

--- src/CC/CCLogin.jsx
+++ src/CC/CCLogin.jsx
@@ -1,27 +1,13 @@
 import React, { Component } from 'react'
 import '../style.css'
-import fire from '../config/fire';
+
 
 export default class CCLogin extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            user: null,
-            passwordInput: '',
-            warningMessage: ''
+
         }
-    }
-    getUsername = (e) => { this.setState({ user: e.target.value }) }
-    getUserPassword = (e) => { this.setState({ passwordInput: e.target.value }) }
-
-
-
-
-    btnLogin = () => {
-        let user = this.state.user;
-        let password = this.state.passwordInput;
-
-        fire.auth().signInWithEmailAndPassword(user, password).then(u => console.log("Good")).catch((err) => console.log("Error") + err.toString())
     }
 
     render() {
@@ -31,10 +17,10 @@ export default class CCLogin extends Component {
                     <div id="childOne"><div><img width="50px" src="ProjectManagerLogo.png"></img><p>&nbsp; Project Manager</p></div><h1>Welcome back</h1></div>
                     <div id="childTwo">
                         <form>
-                            <p>Email</p>
-                            <input type="text" placeholder="Username" onChange={this.getUsername}></input>
+                            <p>Email Address</p>
+                            <input type="text" placeholder="Username" onChange={}></input>
                             <p>Password</p>
-                            <input type="password" placeholder="Password" onChange={this.getUserPassword}></input>
+                            <input type="password" placeholder="Password" onChange={}></input>
                         </form>
                         <button onClick={this.btnLogin}>Next</button>
                     </div>
