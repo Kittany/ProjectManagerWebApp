624891b09d16ef55d93fccef7541a7816534fa2b
Hakam Mssarwe <HakamStudent@gmail.com>
2021-02-06 19:45:42 +0200

    Removed database

--- src/CC/CCLogin.jsx
+++ src/CC/CCLogin.jsx
@@ -14,18 +14,18 @@ export default class CCLogin extends Component {
         return (
             <div id="CCLogin">
                 <div className="loginDivChild">
-                    <div id="childOne"><div><img width="50px" src="ProjectManagerLogo.png"></img><p>&nbsp; Project Manager</p></div><h1>Welcome back</h1></div>
+                    <div id="childOne"><div><img width="50px" src="ProjectManagerLogo.png" alt=""></img><p>&nbsp; Project Manager</p></div><h1>Welcome back</h1></div>
                     <div id="childTwo">
                         <form>
                             <p>Email Address</p>
-                            <input type="text" placeholder="Username" onChange={}></input>
+                            <input type="text" placeholder="Username"></input>
                             <p>Password</p>
-                            <input type="password" placeholder="Password" onChange={}></input>
+                            <input type="password" placeholder="Password"></input>
                         </form>
                         <button onClick={this.btnLogin}>Next</button>
                     </div>
                 </div>
-                <div className="loginDivChild"><img width="100%" height="40%" src="loginPageImage.png"></img></div>
+                <div className="loginDivChild"><img width="100%" height="40%" src="loginPageImage.png" alt=""></img></div>
             </div>
         )
     }
