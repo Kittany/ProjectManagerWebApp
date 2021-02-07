6eb48a2f57e1d7fa079f494a5a4e32a8365ae6af
Mohamed Kittany <m_love_1999@hotmail.com>
2021-02-07 07:26:35 +0200

    save

--- src/CC/CCLogin.jsx
+++ src/CC/CCLogin.jsx
@@ -1,14 +1,33 @@
-import React, { Component } from 'react'
-import '../style.css'
-
+import React, { Component, useRef, useState } from 'react';
+import '../style.css';
+import { AuthProvider } from '../contexts/AuthContext';
 
 export default class CCLogin extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            
+            emailInput: "",
+            passwordInput: "",
+            error: "",
+
+        }
+
+    }
+    handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            this.setState({ error: "" })
+            this.setState({ loading: true })
+            await AuthProvider.login(this.state.emailInput, this.state.passwordInput)
+            console.log("working");
+        } catch {
+            console.log("not");
         }
     }
+
+    getEmailAdress = (e) => { this.setState({ emailInput: e.target.value }) }
+    getPassword = (e) => { this.setState({ passwordInput: e.target.value }) }
 
     render() {
         return (
@@ -16,13 +35,13 @@ export default class CCLogin extends Component {
                 <div className="loginDivChild">
                     <div id="childOne"><div><img width="50px" src="ProjectManagerLogo.png" alt=""></img><p>&nbsp; Project Manager</p></div><h1>Welcome back</h1></div>
                     <div id="childTwo">
-                        <form>
+                        <form onSubmit={this.handleSubmit}>
                             <p>Email Address</p>
-                            <input type="text" placeholder="Username"></input>
+                            <input type="text" placeholder="Username" onChange={this.getEmailAdress} ></input>
                             <p>Password</p>
-                            <input type="password" placeholder="Password"></input>
+                            <input type="password" placeholder="Password" onChange={this.getPassword}></input>
                         </form>
-                        <button onClick={this.btnLogin}>Next</button>
+                        <button type="submit">Next</button>
                     </div>
                 </div>
                 <div className="loginDivChild"><img width="100%" height="40%" src="loginPageImage.png" alt=""></img></div>
