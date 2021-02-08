ec2aa9ceaede249828d1c30796afdfcce95fd4cb
Mohamed Kittany <m_love_1999@hotmail.com>
2021-02-08 17:17:45 +0200

    Waiting for the fixed version

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
       {/* <CCUserProjects/> */}
       {/* <CCAdminPage/>   */}
       {/* <CCUserProfile/> */}
-      {/* <CCSettingsPage/> */}
-      <CCProjectDetail/>
+      <CCSettingsPage/>
+      {/* <CCProjectDetail/> */}
     </div>
   );
 }
